65ae1c7e938648e75770d70cc9ae3a23d64e1fd2
Douglas Hoang <douglashoang@Douglass-MacBook-Pro.local>
2019-01-25 12:53:43 -0600

    cleaning up the files

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,19 +1,97 @@
 // @flow
 import React, { Component } from 'react';
+import Keyboard from 'react-simple-keyboard';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
-import styles from './Home.css';
+import { Button } from 'antd';
+import './Home.css';
+import './index.css';
 
-type Props = {};
+export default class Home extends Component {
+    state = {
+    layoutName: "default",
+    input: ""
+  };
 
-export default class Home extends Component<Props> {
-  props: Props;
+  onChange = input => {
+    this.setState({
+      input: input
+    });
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = button => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+  };
+
+  handleShift = () => {
+    let layoutName = this.state.layoutName;
+
+    this.setState({
+      layoutName: layoutName === "default" ? "shift" : "default"
+    });
+  };
+
+  onChangeInput = event => {
+    let input = event.target.value;
+    this.setState(
+      {
+        input: input
+      },
+      () => {
+        this.keyboard.setInput(input);
+      }
+    );
+  };
 
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+      <div>
+        <Button type="primary">Hello</Button>
+        <input
+          value={this.state.input}
+          placeholder={"Tap on the virtual keyboard to start"}
+          onChange={e => this.onChangeInput(e)}
+        />
+        <Keyboard
+          ref={r => (this.keyboard = r)}
+          onChange={input => this.onChange(input)}
+          onKeyPress={button => this.onKeyPress(button)}
+          theme={"hg-theme-default hg-layout-default myTheme"}
+          layoutName={this.state.layoutName}
+          layout={{
+            default: [
+              "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+              "{tab} q w e r t y u i o p [ ] \\",
+              "{lock} a s d f g h j k l ; ' {enter}",
+              "{shift} z x c v b n m , . / {shift}",
+              ".com @ {space}"
+            ],
+            shift: [
+              "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+              "{tab} Q W E R T Y U I O P { } |",
+              '{lock} A S D F G H J K L : " {enter}',
+              "{shift} Z X C V B N M < > ? {shift}",
+              ".com @ {space}"
+            ]
+          }}
+          buttonTheme={[
+            {
+              class: "hg-red",
+              buttons: "Q W E R T Y q w e r t y"
+            },
+            {
+              class: "hg-highlight",
+              buttons: "Q q"
+            }
+          ]}
+        />
+        <div>WTF</div>
       </div>
     );
   }
